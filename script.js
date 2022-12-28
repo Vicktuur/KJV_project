e154460bcd332dc9d3a9cffcd7c98391059f69d6
Abiye Iniabere <104025786+Vicktuur@users.noreply.github.com>
2022-12-28 13:44:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const hamburger = document.querySelector('.hamburger')
+const navigation = document.querySelector('.nav')
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active")
+  navigation.classList.toggle("active")
+})
+
+document.querySelectorAll(".nav-link").forEach(e => e.addEventListener("click", () => {
+    // hamburger.classList.remove("active")
+    // navigation.classList.remove("active")
+  })
+)
+
+let images = [
+    "images/IMG-20221023-WA0001.jpg",
+    "images/IMG_8455.jpeg",
+    "images/IMG_20221218_102723_939.jpg",
+    "images/IMG_8608.JPEG",
+    "images/FB_IMG_1666869596478.jpg"
+];
+ 
+let num = 0;
+
+function prev() {
+    let slider = document.getElementById("slider");
+    num--;
+    if(num < 0) {
+        num = images.length-1;
+    }
+    slider.src = images[num];
+}
+
+function next() {
+    let slider = document.getElementById("slider");
+    num++;
+    if(num >= images.length) {
+        num = 0;
+    }
+    slider.src = images[num];
+}
+
+setInterval(function() {
+    let random = Math.floor(Math.random() * 5);
+    slider.src = images[random]
+}, 10000);
+
+const navl = document.querySelectorAll(".mo");
+        navl.forEach((navl) => {
+            navl.addEventListener("click", () => {
+                navl.classList.toggle("active");
+            });
+        });
---
+++ scriptpastors.js
@@ -0,0 +1,13 @@
+let image = [
+    "images/FB_IMG_1604304554429.jpg",
+    "images/FB_IMG_1633071606091.jpg",
+    "images/FB_IMG_1604304777680.jpg",
+    "images/IMG-20221023-WA0004.jpg"
+];
+
+let pstpics = document.getElementById("pst-pics");
+
+setInterval(function() {
+    let random = Math.floor(Math.random() * 4);
+    pstpics.src = image[random]
+}, 7000);
